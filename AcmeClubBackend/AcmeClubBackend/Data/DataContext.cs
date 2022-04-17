cff6e926fdefd186309c06e2cab696813873901f
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-04-17 17:34:44 -0400

    Db migrated

--- AcmeClubBackend/AcmeClubBackend/Data/DataContext.cs
+++ AcmeClubBackend/AcmeClubBackend/Data/DataContext.cs
@@ -1,6 +1,13 @@
-﻿namespace AcmeClubBackend.Data
+﻿using AcmeClubBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AcmeClubBackend.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+            public DbSet<Registration> Registrations { get; set; }
+           
     }
 }
--- AcmeClubBackend/AcmeClubBackend/Models/Registration.cs
+++ AcmeClubBackend/AcmeClubBackend/Models/Registration.cs
@@ -6,7 +6,7 @@
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public DateOnly StartDate { get; set; } 
+        public DateTime StartDate { get; set; } 
         public string YearsOfExperience { get; set; } = string.Empty;
         public string Activity { get; set; } = string.Empty;
         public string Comments { get; set; } = string.Empty;
