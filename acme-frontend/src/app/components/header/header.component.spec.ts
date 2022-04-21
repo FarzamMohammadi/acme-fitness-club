638541acd391e10a609af6953381d284771851f1
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-04-20 21:03:27 -0400

    Frontend - Tests added to form & header component

--- acme-frontend/src/app/components/header/header.component.spec.ts
+++ acme-frontend/src/app/components/header/header.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { HeaderComponent } from './header.component';
 
@@ -8,9 +10,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      providers: [By],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +23,14 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Customized tests
+  it('title should change', () => {
+    const title = 'Welcome Home!';
+    component.title = title;
+    fixture.detectChanges();
+    let h1Title = fixture.debugElement.query(By.css('h1'));
+    expect(h1Title.context.title).toEqual(title);
   });
 });
--- acme-frontend/src/app/components/registration-form/registration-form.component.spec.ts
+++ acme-frontend/src/app/components/registration-form/registration-form.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistrationFormComponent } from './registration-form.component';
 
 describe('RegistrationFormComponent', () => {
@@ -20,5 +19,56 @@ describe('RegistrationFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  // Customized test
+  it('check form contorls of form', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const firstName: HTMLInputElement =
+        fixture.nativeElement.querySelector('firstName');
+      const lastName: HTMLInputElement =
+        fixture.nativeElement.querySelector('lastName');
+      const email: HTMLInputElement =
+        fixture.nativeElement.querySelector('email');
+      const yearsOfExperience: HTMLInputElement =
+        fixture.nativeElement.querySelector('yearsOfExperience');
+      const activity: HTMLInputElement =
+        fixture.nativeElement.querySelector('activity');
+      const comments: HTMLInputElement =
+        fixture.nativeElement.querySelector('comments');
+
+      firstName.value = 'Farzam';
+      lastName.value = 'Mo';
+      email.value = 'Farzam@gmail.com';
+      yearsOfExperience.value = '2';
+      activity.value = 'Swimming';
+      comments.value = 'Fitness is fun.';
+
+      firstName.dispatchEvent(new Event('input'));
+      lastName.dispatchEvent(new Event('input'));
+      email.dispatchEvent(new Event('input'));
+      yearsOfExperience.dispatchEvent(new Event('input'));
+      activity.dispatchEvent(new Event('input'));
+      comments.dispatchEvent(new Event('textarea'));
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(component.firstName).toEqual(firstName.value);
+        expect(component.lastName).toEqual(lastName.value);
+        expect(component.email).toEqual(email.value);
+        expect(component.yearsOfExperience).toEqual(yearsOfExperience.value);
+        expect(component.comments).toEqual(comments.value);
+      });
+    });
+  });
+
+  it('should captialize all words', () => {
+    const unfilteredSentence = 'lEtS Test thiS sentence';
+    const filteredSentence = 'Lets Test This Sentence';
+    fixture.detectChanges();
+    expect(component.capitalizeFirstLetter(unfilteredSentence)).toEqual(
+      filteredSentence
+    );
   });
 });
